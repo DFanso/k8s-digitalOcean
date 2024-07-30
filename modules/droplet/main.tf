0e11927e369e8eb88545c419617d98df4e87c3d6
Leo Gavin <leogavin123@outlook.com>
2024-07-30 14:40:10 +0530

    Update main.tf

--- modules/droplet/main.tf
+++ modules/droplet/main.tf
@@ -25,9 +25,24 @@ resource "digitalocean_firewall" "default" {
     content {
       protocol   = inbound_rule.value.protocol
       port_range = inbound_rule.value.port_range
-      source_addresses = ["0.0.0.0/0", "::/0"]
+      source_addresses = ["0.0.0.0/0"]
     }
   }
+
+
+  outbound_rule {
+    protocol   = "tcp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol   = "udp"
+    port_range = "all"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+
 }
 
 resource "digitalocean_project_resources" "default" {
