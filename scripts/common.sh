14dc104d5dfa92a5e9ba553a70603f3d38367532
EmpiteKushan <109583247+EmpiteKushan@users.noreply.github.com>
2024-07-31 08:33:11 +0530

    Updated the Kubeadm, Kubelet, Kubectl installation commands.

--- scripts/common.sh
+++ scripts/common.sh
@@ -66,6 +66,7 @@ echo "CRI runtime installed susccessfully"
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
+sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
